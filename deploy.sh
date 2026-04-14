ca4d08b034210ad47ee5644c22d4251ef9773d35
Filipe DEV <filipedomingos198@gmail.com>
2026-04-14 15:01:26 +0200

    fix: remover rotas duplicadas stock-movements

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Deploy script para o servidor de produção
-SERVER="ubunto@146.235.224.99"
-KEY=".ssh/oracle2025"
+SERVER="ubuntu@146.235.224.99"
+KEY="/home/fdev-ms/.ssh/oracle-2025"
 PROJECT_DIR="/var/www/html/reprosys"
 
 echo "🚀 Iniciando deploy para produção..."
 
 ssh -i $KEY $SERVER "cd $PROJECT_DIR && \
+    echo '🔧 Corrigindo permissões do git...' && \
+    sudo chown -R ubuntu:ubuntu .git && \
     echo '📥 Puxando últimas alterações...' && \
     git pull origin main && \
     echo '📦 Instalando dependências...' && \
